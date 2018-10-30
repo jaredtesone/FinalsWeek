14db4474d410a3e431f2045e29465b18528ec610
Jared Tesone <nyrfanjp3@gmail.com>
2018-10-30 15:49:15 -0400

    Preliminary player movement with forces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public Rigidbody rb;
+    public int forceMult = 5;
+       
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    private void FixedUpdate() {
+        /*if ((Input.GetKey("w") || Input.GetKey("up")) && (Input.GetKey("s") || Input.GetKey("down"))) {
+          rb.AddForce(Vector3.zero);
+        }*/
+        if (Input.GetKey("w") || Input.GetKey("up")){
+            //move forward
+            rb.AddForce(5*Vector3.forward);
+        }
 
-    void FixedUpdate ()
-    {
-        Input
+        if (Input.GetKey("s") || Input.GetKey("down")){
+            //move back
+            rb.AddForce(5*Vector3.back);
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left")){
+            //move left
+            rb.AddForce(5*Vector3.left);
+        }  
+        
+        if (Input.GetKey("d") || Input.GetKey("right")){
+            //move right
+            rb.AddForce(5*Vector3.right);
+        }
     }
 
 }
