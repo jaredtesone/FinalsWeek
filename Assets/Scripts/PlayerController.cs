e0594a644cbcf48e555f81adff8181d04d18b619
Jared Tesone <nyrfanjp3@gmail.com>
2018-11-01 19:22:17 -0400

    Modified Player movement to be less sluggish

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,36 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public Rigidbody rb;
-    public int forceMult = 5;
+    private Rigidbody rb;
+    public float speed = 3;
+    public float timeChange = 10;
+    //public Transform player;
        
     private void Start() {
         rb = GetComponent<Rigidbody>();
     }
     
     private void FixedUpdate() {
-        /*if ((Input.GetKey("w") || Input.GetKey("up")) && (Input.GetKey("s") || Input.GetKey("down"))) {
-          rb.AddForce(Vector3.zero);
-        }*/
-        if (Input.GetKey("w") || Input.GetKey("up")){
-            //move forward
-            rb.AddForce(5*Vector3.forward);
-        }
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // //move according to input
+        //rb.AddForce(speed * movement);
+        //Vector3 desiredDirection = transform.position - player.transform.position;
+        rb.velocity = Vector3.Lerp(rb.velocity, speed * movement, timeChange * Time.deltaTime);
 
-        if (Input.GetKey("s") || Input.GetKey("down")){
-            //move back
-            rb.AddForce(5*Vector3.back);
-        }
+        //if (Input.GetKey("w") || Input.GetKey("up"))
+        //{
+        //    //move forward
+        //    rb.AddForce(5 * Vector3.forward);
+        //}
 
-        if (Input.GetKey("a") || Input.GetKey("left")){
-            //move left
-            rb.AddForce(5*Vector3.left);
-        }  
-        
-        if (Input.GetKey("d") || Input.GetKey("right")){
-            //move right
-            rb.AddForce(5*Vector3.right);
-        }
+        //if (Input.GetKey("s") || Input.GetKey("down"))
+        //{
+        //    //move back
+        //    rb.AddForce(5 * Vector3.back);
+        //}
+
+        //if (Input.GetKey("a") || Input.GetKey("left"))
+        //{
+        //    //move left
+        //    rb.AddForce(5 * Vector3.left);
+        //}
+
+        //if (Input.GetKey("d") || Input.GetKey("right"))
+        //{
+        //    //move right
+        //    rb.AddForce(5 * Vector3.right);
+        //}
     }
 
 }
