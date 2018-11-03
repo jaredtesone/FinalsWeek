d39caad2f5706062a0978abe461d2b3925bcb9e3
Unknown <ruofu.sun@gmail.com>
2018-11-03 16:31:02 -0400

    Nov.3  project Camera follow player rotation Spot light added(need to be improved)

---
+++ FinalWeek 11-3/FinalsWeek/Assets/Scripts/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    [SerializeField]
+    GameObject Player;
+    public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = transform.position;
+        position.x = Mathf.Lerp(transform.position.x, Player.transform.position.x, interpolation);
+        position.y = Mathf.Lerp(transform.position.y, Player.transform.position.y+1, interpolation);
+        position.z = Mathf.Lerp(transform.position.z, Player.transform.position.z, interpolation);
+        transform.position = position;
+    }
+}
