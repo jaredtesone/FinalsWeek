1a4d88bbe49cf777ac36832fae37f9972426ac5c
Jared Tesone <nyrfanjp3@gmail.com>
2018-11-04 13:30:55 -0500

    Improved player controls
    Also added some standard asset camera scripts

--- FinalWeek 11-3/FinalsWeek/Assets/Scripts/PlayerController.cs
+++ FinalWeek 11-3/FinalsWeek/Assets/Scripts/PlayerController.cs
@@ -3,49 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    private Rigidbody rb;
-    public float speed = 3;
-    public float timeChange = 10;
+    //private Rigidbody rb;
+    public float speed;
+    public float timeChange;
+    public float rotateSpeed;
+    public float cw;
+    public float ccw;
+    Vector3 movement;
+    Vector3 facing;
     //public Transform player;
        
     private void Start() {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        facing = transform.forward;
     }
     
     private void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        // //move according to input
-        //rb.AddForce(speed * movement);
-        //Vector3 desiredDirection = transform.position - player.transform.position;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.1F);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        if (moveVertical > 0)
+            movement = transform.forward;
+        else if (moveVertical < 0)
+            movement = -transform.forward;
+        else
+            movement = Vector3.zero;
+
+        //if (moveHorizontal > 0)             //turn right
+        //    facing += transform.right;
+        //else if (moveHorizontal < 0)        //turn left
+        //    facing -= transform.right;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(facing), Time.deltaTime * rotateSpeed);
+
+        if (moveHorizontal > 0) {
+            //turn right (clockwise)
+            transform.Rotate(0, Time.deltaTime * cw, 0);
+        } else if (moveHorizontal < 0) {
+            //turn left (counterclockwise)
+            transform.Rotate(0, Time.deltaTime * ccw, 0);
+        }
+
+        //move forward or backward
+        if (moveVertical != 0)
+            transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
        // rb.velocity = Vector3.Lerp(rb.velocity, speed * movement, timeChange * Time.deltaTime);
 
-        //if (Input.GetKey("w") || Input.GetKey("up"))
-        //{
-        //    //move forward
-        //    rb.AddForce(5 * Vector3.forward);
-        //}
-
-        //if (Input.GetKey("s") || Input.GetKey("down"))
-        //{
-        //    //move back
-        //    rb.AddForce(5 * Vector3.back);
-        //}
-
-        //if (Input.GetKey("a") || Input.GetKey("left"))
-        //{
-        //    //move left
-        //    rb.AddForce(5 * Vector3.left);
-        //}
-
-        //if (Input.GetKey("d") || Input.GetKey("right"))
-        //{
-        //    //move right
-        //    rb.AddForce(5 * Vector3.right);
-        //}
+        
     }
 
 }
